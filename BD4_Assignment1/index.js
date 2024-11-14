41d80df89f6590d4aca7a9db3912dc2cb4856d0c
null <null>
2024-11-14 08:20:44 +0530

    Fixing the code for the Port

--- BD4_Assignment1/index.js
+++ BD4_Assignment1/index.js
@@ -26,7 +26,7 @@ const express = require('express');
 const { resolve } = require('path');
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 app.use(express.static('static'));
 const cors=require('cors');
